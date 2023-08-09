acab8554765d549145a96a292def6ebb97be873f
sruknuy <sruknuy@myseneca.ca>
2023-08-09 02:53:54 -0400

    version

---
+++ js/data.js
@@ -0,0 +1,41 @@
+
+// Attach reviewData to the global window Object
+// so other scripts can access it at runtime.
+window.reviewData = [
+    /* Your Review Data Goes Here… */
+    {
+        name: "Alex Cora",
+        date: "2023-08-01",
+        rating: 4,
+        review: "War is a classic card game that I enjoyed playing with my friends. I loved it!"
+      },
+
+      {
+        name: "Emily Dobson",
+        date: "2023-08-02",
+        rating: 5,
+        review: "I absolutely love playing War! It's a fantastic two-player game that's easy to learn and quick to play. Perfect for a casual game night."
+      },
+
+      {
+        name: "Mark Strong",
+        date: "2023-08-03",
+        rating: 3,
+        review: "War is a decent card game for passing the time. It's not very strategic, but it's good for a quick round or two when you have a few minutes to spare."
+      },
+
+      {
+        name: "Sarah Gadon",
+        date: "2023-08-04",
+        rating: 2,
+        review: "I found War to be quite repetitive and luck-based. It lacks depth and strategy, which makes it less appealing to me as a seasoned card game player."
+      },
+
+      {
+        name: "Jason Marz",
+        date: "2023-08-05",
+        rating: 5,
+        review: "War is a classic card game that never gets old. It's perfect for a fun and light-hearted gaming session with friends or family. A must-have in any game collection!"
+      }
+
+    ];
---
+++ js/scripts.js
@@ -0,0 +1,133 @@
+// Create local variables to work with it in this file.
+const {reviewData} = window;
+
+// Select if to display the cards
+const displayRowCards = document.querySelector(".row-cards");
+
+// Create var for review cars////
+const userReview= reviewData.filter(function (data) {
+  return data.name;
+});
+
+
+///////////// Main entry /////////
+// call function to display the review card
+createReviewCard(userReview);
+
+
+// Function to display cards
+function createReviewCard(review) {
+    const getAllReviews = review.map(function(data) 
+      {
+        return getReviewData(data);
+      })
+      .join("");
+  
+    displayRowCards.innerHTML = getAllReviews;
+  }
+
+// Get attributes from reviewDta objct
+function getReviewData(data) 
+{
+    // Convert the rating number to stars
+    const star= '★'.repeat(data.rating) + '☆'.repeat(5 - data.rating);
+
+    const reviewDate = new Date(data.date);
+    const newFormatDate = reviewDate.toLocaleString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
+    
+    return `<div class="review-card">
+      <div class='user-review'>
+        <h4>${data.name}</h4>
+        <time datetime="${data.date}">${newFormatDate}</time><br>
+        <span id="rating">Rating: ${star}</span>
+        <p>"${data.review}"</p>
+      </div>
+    </div>`;
+}
+
+// Attach event listener to "Create" button
+var button = document.getElementById('create-button');
+
+button.addEventListener('click', function() {
+
+  const addName = document.getElementById('name').value;
+  const addDate = document.getElementById('date').value;
+  const addRating = parseInt(document.getElementById('rating-input').value);
+  const addReview = document.getElementById('review-text').value;
+
+  // validate a new input
+  if (isNaN(addRating) || addRating < 1 || addRating > 5 || !addReview || !addName || !addDate)
+  {
+    alert("Sorry, Please complete all fields.");
+
+    
+  }else // if the input is valid
+  {
+    const newReviewObject = {
+      name: addName,
+      date: addDate,
+      rating: addRating,
+      review: addReview
+  };
+  
+  // add a new object
+  reviewData.push(newReviewObject);
+
+  // Clear the id
+  displayRowCards.innerHTML = " "
+
+   // Display the new card immediately after adding
+   createReviewCard(reviewData);
+
+   // Clear form inputs
+   document.getElementById('name').value = '';
+   document.getElementById('date').value = '';
+   document.getElementById('rating').value = '';
+   document.getElementById('review-text').value = '';
+
+
+  }
+
+
+});
+
+
+
+/*
+// Attach event listener to "Create" button
+document.getElementById('create-button').addEventListener('click', newReview);
+
+
+// Function to add a new review
+function newReview() {
+    const addName = document.getElementById('name').value;
+    const addDate = document.getElementById('date').value;
+    const addRating = parseInt(document.getElementById('rating').value);
+    const addReview = document.getElementById('review-text').value;
+
+    const newReviewObject = {
+        name: addName,
+        date: addDate,
+        rating: addRating,
+        review: addReview
+    };
+    
+    // add a new object
+    reviewData.push(newReviewObject);
+
+    // Clear the id
+    displayRowCards.innerHTML = " "
+
+     // Display the new card immediately after adding
+     createReviewCard(reviewData);
+
+     // Clear form inputs
+     document.getElementById('name').value = '';
+     document.getElementById('date').value = '';
+     document.getElementById('rating').value = '';
+     document.getElementById('review-text').value = '';
+
+}
+
+*/
+
